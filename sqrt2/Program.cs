b8feb6cd83441c45496d7746c90d2eecd2bcdb79
senpa <senpa@DESKTOP-1D8OBO7>
2024-05-14 11:29:40 +0300

    Добавьте файлы проекта.

---
+++ sqrt2/Program.cs
@@ -0,0 +1,28 @@
+﻿static double Sqrt(double x)
+{
+    if (x == 0) 
+    {
+        return 0;
+    }
+
+    double approximation = x; 
+    double tolerance = 0.0001; 
+
+    while (true)
+    {
+        double nextApproximation = 0.5 * (approximation + x / approximation); 
+        if (Math.Abs(approximation - nextApproximation) < tolerance)
+        {
+            break; 
+        }
+        approximation = nextApproximation;
+    }
+
+    return approximation;
+}
+
+
+double number = 100;
+double squareRoot = Sqrt(number);
+Console.WriteLine("Квадратный корень из " + number + " = " + squareRoot);
+
---
+++ x = cos x/Program.cs
@@ -0,0 +1,11 @@
+﻿double x0 = 0; 
+double x;
+do
+{
+    x = Math.Cos(x0); 
+    x0 = x;
+}
+while (Math.Abs(x - x0) > 0.000001); 
+
+Console.WriteLine("Решение уравнения: x = " + x);
+
